bf51de5c751b7558a3842afc2249bd30a2a174fc
Szabó Attus <szabo.attila.10@gmail.com>
2017-02-02 10:57:37 +0100

    UDPServer refactored, finished ProofOfConcept

---
+++ UDPServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace UDPServer {
+  class UDPServer
+  {
+    private const int listenPort = 7777;
+    private static void StartServer()
+    {
+      bool isQuit = false;
+      UdpClient listener = new UdpClient(listenPort);
+      List<IPEndPoint> endPointList = new List<IPEndPoint>();
+      IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+      try
+      {
+        while (!isQuit)
+        {
+          byte[] bytes = listener.Receive(ref clientEndPoint);
+          if (!endPointList.Contains(clientEndPoint))
+          {
+            endPointList.Add(clientEndPoint);
+          }
+          Console.WriteLine("Received broadcast from {0} :\n {1}\n", clientEndPoint.ToString(), Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+          foreach (IPEndPoint item in endPointList)
+          {
+            if (!item.Equals(clientEndPoint))
+            {
+              listener.Send(bytes, bytes.Length, item);
+            }
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+      }
+      finally
+      {
+        listener.Close();
+      }
+    }
+
+    public static int Main()
+    {
+      StartServer();
+      return 0;
+    }
+  }
+}
