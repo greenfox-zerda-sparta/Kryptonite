864cb0f1ae26795aa5f3e5c06f1619531c93fb3d
Szabó Attus <szabo.attila.10@gmail.com>
2017-02-02 14:26:14 +0100

    MazeGame scripts

---
+++ MazeGameScripts/MenuHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+  public void exitProgramBtn() {
+    Application.Quit();
+  }
+
+  public void NewGameBtn(string PlayGame) {
+    SceneManager.LoadScene(PlayGame);
+  }
+
+  public void NewHelperGameBtn(string PlayGameHelper)
+  {
+    SceneManager.LoadScene(PlayGameHelper);
+  }
+}
---
+++ MazeGameScripts/Player2Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// uses methods from PlayerController: UDPconnection
+
+public class Player2Move : MonoBehaviour {
+  private float speed;
+  private Rigidbody rd;
+  private Vector3 spawnPosition = new Vector3(2, 1, 2);
+  private UDPconnection uc;
+
+  private void Start()
+  {
+    uc = new UDPconnection();
+    uc.SendData("Player - Navigator Connected");
+    rd.position = spawnPosition;
+  }
+
+  private void FixedUpdate()
+  {
+    string str = uc.incomeData();
+    spawnPosition.x = float.Parse(uc.SplitData(str)[0]);
+    spawnPosition.z = float.Parse(uc.SplitData(str)[1]);
+    rd.position = spawnPosition;
+  }
+}
