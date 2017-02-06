8456566c5b2e5f6affc69d1eacbe5a61a72bc249
Szabó Attus <szabo.attila.10@gmail.com>
2017-02-06 16:11:48 +0100

    GameServer class created, - optional server starter program /later we can handle the threads from this program/

---
+++ GameServer/ServerSelector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+
+namespace GameServer
+{
+  class ServerSelector
+  {
+    private string arg;
+
+    public ServerSelector()
+    {
+      arg = "";
+    }
+
+    private void inputArg()
+    {
+      Console.WriteLine("Write -u- for UDP Server start\nWrite -t- for TCP Server start\nWrite -all- to start both\nType -exit- for quit");
+      arg = Console.ReadLine();
+    }
+
+    public void select()
+    {
+      inputArg();
+      switch (arg)
+      {
+        case "all":
+          Console.WriteLine("Both starting...");
+          break;
+        case "u":
+          Console.WriteLine("UDP starting...");
+          break;
+        case "t":
+          Console.WriteLine("TCP starting...");
+          break;
+        case "exit":
+          Console.WriteLine("Bye!  ^.^");
+          System.Environment.Exit(1);
+          break;
+        default:
+          Console.WriteLine("Wrong arguments");
+          select();
+          break;
+      }
+    }
+  }
+}
---
+++ GameServer/UdpServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace GameServer
+{
+  class UDPServer
+  {
+    
+  }
+}
