b0eed2aad47b1f67b3d6a336255c28e657d237f3
Nagy Zsuzsanna <nagyzsuska@live.com>
2017-02-12 19:57:52 +0100

    PracticingUnitTest

---
+++ CSProjectForPracticeUnitTest/CSProjectForPracticeUnitTest/ClassForDiffMethods.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjectForPracticeUnitTest {
+  public class ClassForDiffMethods {
+    private int number1;
+    private int number2;
+
+    public int Number1
+    {
+      get
+      {
+        return number1;
+      }
+
+      set
+      {
+        number1 = value;
+      }
+    }
+
+    public int Number2
+    {
+      get
+      {
+        return number2;
+      }
+
+      set
+      {
+        number2 = value;
+      }
+    }
+
+    public ClassForDiffMethods() { }
+
+    public int add(int n1, int n2)
+    {
+      int sum = 0;
+      sum = n1 + n2;
+      return sum;
+    }
+
+    public double divide(int n1, int n2)
+    {
+      if (n2 == 0)
+      {
+        throw new DivideByZeroException();
+      }
+      else
+      {
+        double result = n1 / n2;
+        return result;
+      }
+    }
+    
+    public bool isBigger(int n1, int n2)
+    {
+      return n1 > n2;
+    }
+  }
+}
---
+++ CSProjectForPracticeUnitTest/CSProjectForPracticeUnitTestTests/ClassForDiffMethodsTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSProjectForPracticeUnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjectForPracticeUnitTest.Tests {
+  [TestClass()]
+  public class ClassForDiffMethodsTests {
+    [TestMethod()]
+    public void addTest_is_give_back_a_good_sum()
+    {
+      int n1 = 2;
+      int n2 = 2;
+      int expected = 4;
+      var sg = new ClassForDiffMethods();
+      int actual = sg.add(n1, n2);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void addTest_with_wrong_sum()
+    {
+      int n1 = 2;
+      int n2 = 2;
+      int notExpected = 3;
+      var sg = new ClassForDiffMethods();
+      int actual = sg.add(n1, n2);
+      Assert.AreNotEqual(notExpected, actual);
+    }
+
+    [TestMethod()]
+    [Timeout(1000)]
+    public void divideTest()
+    {
+      //int n1 = 2;
+      //int n2 = 2;
+      //int expected = 1;
+      var sg = new ClassForDiffMethods();
+      //double actual = sg.divide(n1, n2);
+      Assert.AreEqual(1, sg.divide(2, 2));
+      Assert.AreEqual(1, sg.divide(-2, -2));
+      Assert.AreEqual(5, sg.divide(25, 5));
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(DivideByZeroException))]
+    public void divideTestWithExeption()
+    {
+      var sg = new ClassForDiffMethods();
+      sg.divide(2, 0);
+    }
+
+    [TestMethod()]
+    [Timeout(TestTimeout.Infinite)]
+    public void isBiggerTest_true()
+    {
+      var sg = new ClassForDiffMethods();
+      Assert.IsTrue(sg.isBigger(3, 2));
+    }
+
+    [TestMethod()]
+    public void isBiggerTest_false()
+    {
+      var sg = new ClassForDiffMethods();
+      Assert.IsFalse(sg.isBigger(2, 4));
+    }
+  }
+}
+
