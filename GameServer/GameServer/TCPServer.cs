2cd552f456408d35266ad36ceccf54b309fc6090
Nagy Zsuzsanna <nagyzsuska@live.com>
2017-02-14 08:23:27 +0100

    TCPServer's refactoring is in progress

--- GameServer/GameServer/TCPServer.cs
+++ GameServer/GameServer/TCPServer.cs
@@ -9,51 +9,58 @@ namespace GameServer
 {
   public class TCPServer
   {
-    public static ManualResetEvent allDone = new ManualResetEvent(false);
-    public static List<Socket> socketList;
     private const int PORT = 5555;
+    private static ManualResetEvent allDone;
+    private static List<Socket> socketList;
     private static MazeGenerator mazeGen;
+    private static IPEndPoint localEndPoint;
+    private static Socket listener;
+    private static string content;
+    private static int bytesRead;
 
     public TCPServer()
     {
+      allDone = new ManualResetEvent(false);
+      localEndPoint = new IPEndPoint(IPAddress.Any, PORT);
+      listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      socketList = new List<Socket>();
+      content = String.Empty;
       mazeGen = new MazeGenerator();
     }
 
     public void StartTcpServer()
     {
-      IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, PORT);
-      Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-      socketList = new List<Socket>();
-
       try
       {
-        listener.Bind(localEndPoint);
-        listener.Listen(100);
-        while (true)
-        {
-          allDone.Reset();
-          Console.WriteLine("Waiting for a connection...");
-          listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
-          allDone.WaitOne(); 
-
-        }
+        Listening();
       }
       catch (Exception e)
       {
         Console.WriteLine(e.ToString());
       }
       Console.WriteLine("\nPress ENTER to continue...");
-      Console.Read();
+    }
+
+    private static void Listening()
+    {
+      listener.Bind(localEndPoint);
+      listener.Listen(100);
+      while (true)
+      {
+        allDone.Reset();
+        Console.WriteLine("Waiting for a connection...");
+        listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+        allDone.WaitOne();
+      }
     }
 
     public static void AcceptCallback(IAsyncResult ar)
     {
       allDone.Set();
-      Socket listener = (Socket)ar.AsyncState;
+     // Socket listener = (Socket)ar.AsyncState;
       Socket handler = listener.EndAccept(ar);
       socketList.Add(handler);
       BeginRecieve(handler);
-      //sending a list to the players for generate a maze
       Send(handler, mazeGen.ByteArrayOfConvertedWallList);
     }
 
@@ -66,10 +73,9 @@ namespace GameServer
 
     public static void ReadCallback(IAsyncResult ar)
     {
-      string content = String.Empty;
       StateObject state = (StateObject)ar.AsyncState;
       Socket handler = state.workSocket;
-      int bytesRead = 0;
+      bytesRead = 0;
       try
       {
         bytesRead = handler.EndReceive(ar);
@@ -80,38 +86,58 @@ namespace GameServer
       }
       if (bytesRead > 0)
       {
-        state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-        content = state.sb.ToString();
-        if (content.IndexOf("<EOF>") > -1)
-        {
-          TCPMessageID ID = (TCPMessageID)state.buffer[0];
-          switch (ID)
-          {
-            case TCPMessageID.Message:
-              content = state.sb.ToString(0, state.sb.Length - 5);
-              Console.WriteLine(content);              
-              break;
-            case TCPMessageID.Trap:
-              foreach (var item in socketList)
-              {
-                if (!item.Equals(handler))
-                {
-                  Send(item, content);
-                }
-              }
-              break;
-          }
-        }
-        else
-        {
-          handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
-        }
-        state.sb.Clear();
+      TransformIncomeData(state, handler);
+      state.sb.Clear();
+      handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+      }
+    }
+
+
+    private static void TransformIncomeData(StateObject state, Socket handler)
+    {
+      state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+      content = state.sb.ToString();
+      if (content.IndexOf("<EOF>") > -1)
+      {
+        CheckMessageID(state, handler);
+      }
+      else
+      {
         handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
       }
     }
 
-    private static void Send(Socket handler, string data)
+    private static void CheckMessageID(StateObject state, Socket handler)
+    {
+      TCPMessageID ID = (TCPMessageID)state.buffer[0];
+      switch (ID)
+      {
+        case TCPMessageID.Message:
+          PrintMessageToConsol();
+          break;
+        case TCPMessageID.Trap:
+          SendMessageToTheOtherClients(handler);
+          break;
+      }
+    }
+
+    public static void PrintMessageToConsol()
+    {
+      string message = content.Substring(0, content.Length - 5);
+      Console.WriteLine(message);
+    }
+
+    private static void SendMessageToTheOtherClients(Socket handler) { 
+          foreach (var item in socketList)
+          {
+        if (!item.Equals(handler))
+        {
+          Send(item, content);
+        }
+  }
+}
+
+private static void Send(Socket handler, string data)
     {
       byte[] byteData = Encoding.ASCII.GetBytes(data);
       handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
