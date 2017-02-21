6dc873cd954dcb78cca5e27d82511e3cad4815ed
Nagy Zsuzsanna <nagyzsuska@live.com>
2017-02-21 19:34:27 +0100

    ScriptsForReceiveTraps

---
+++ ScriptsForReceiveTraps/StateObject.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class StateObject : MonoBehaviour {
+
+    public Socket workSocket = null;
+    public const int BufferSize = 1024;
+    public byte[] buffer = new byte[BufferSize];
+    public StringBuilder sb = new StringBuilder();
+
+}
+
+public enum TCPMessageID : byte {
+  Message,
+  Maze,
+  Trap,
+  TrapPosition,
+  MazeIsReceived,
+}
